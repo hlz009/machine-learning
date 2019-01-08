0eae983dab3bc5ff2489a8cbc4d633a80e058260
小智 <583172396@qq.com>
2019-01-08 20:54:26 +0800

    lesson001

---
+++ lesson001.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#coding=utf-8
+'''
+主要进行矩阵计算
+'''
+import numpy as np
+
+npArray = np.array([0,2,3,4])
+# print(npArray)
+# print(help(np.array))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
